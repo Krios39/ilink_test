271abb885766259d58bc8d0c24b5fb32550699ec
Krios39 <ryslan12072000@mail.ru>
2021-01-27 18:22:15 +0700

    Залив

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
+import DnDField from "./components/DnDField/DnDField";
+import Title from "./components/Title/Title";
+import TranslatedInformation from "./components/TranslatedInformation/TranslatedInformation";
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+export interface Data{
+  english:string
+  russian:string
+}
 
 function App() {
+
+  const [data,setData] = useState({} as Data)
+
+  useEffect(()=>{
+    axios.get( "http://localhost:3000/translate/")
+        .then((resp)=>setData(resp.data))
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Title size="big" text="Translate this sentence"/>
+        <TranslatedInformation text={data.russian}/>
+        <DnDField english={data.english}/>
+      </div>
   );
 }
 
